794d0eba623ed83288318163d4a3fb6bdaaf464f
marioharo <haromario2@gmail.com>
2025-11-28 00:25:03 -0500

    PA4 done and finish this exam

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('scroll-up-btn');
     btn.addEventListener('click', e => { e.preventDefault(); window.scrollTo({ top: 0, behavior: 'smooth' }); });
-    window.addEventListener('scroll', () => { btn.style.display = window.scrollY > 1500 ? 'block' : 'none'; });
+    window.addEventListener('scroll', () => { btn.style.display = window.scrollY > 50 ? 'block' : 'none'; });
 });
+
+// Menú hamburguesa
+// acceder al boton hamburguesa desde javascript
+var menuToggle = document.querySelector(".menu-toggle");
+
+// acceder al menú (nav-menú) desde javascript
+var navMenu = document.querySelector(".navbar");
+
+// captura el evento click del botón hamburguesa para alternar la visutalización del nemú
+menuToggle.onclick = () =>{
+    navMenu.classList.toggle("show")
+}
